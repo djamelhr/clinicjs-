bc3f10f6525e535bb3aa767b716f62977e5a1d33
djamelhr <30354876+djamelhr@users.noreply.github.com>
2018-02-04 16:59:40 -0800

    delete djo/app.js

--- app.js
+++ app.js
@@ -12,14 +12,37 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride('_method'));
 
+var serviseSchema = new mongoose.Schema({
+  title : String,
+  image : String,
+  body  : String
+});
+var Servise = mongoose.model("Servise", serviseSchema);
+
+Servise.create({
+  title : "dhersa",
+  image : "https://farm6.staticflickr.com/5140/5454249267_5044b3c814.jpg",
+  body : "hello this is"
+});
+
+
 app.get("/",function (req,res) {
   res.render("index");
 });
-app.get("/servise",function (req,res) {
-res.render("servise");
+app.get("/servises",function (req,res) {
+res.render("servises");
 })
 
+app.get("/servises/:id",function (req,res) {
+  Servise.findById( req.params.id , function(err, foundServise) {
+   if(err){
+     res.redirect("/servises");
+   } else {
+     res.render("show", {servise : foundServise}) ;
+   }
+ });
+});
+
 app.listen(3000,process.env.IP,function (){
-  console.log("mechat");
- console.log("doadakok");
+  console.log("mechat")
 });
